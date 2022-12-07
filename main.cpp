a105b98c6cbad687aaa5986cf02379f6af11c64a
palswayam5 <s_pal@es.iitr.ac.in>
2022-12-08 00:27:27 +0530

    started to create framework

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include<iostrema>
+#include<string>
+#include<fstream>
+using namespace std
+
+class User_Login
+{	
+   string username,password;
+   
+   User_Login()
+   {
+	   cout<<"Enter your Username"<<username<<endl;
+	   //search for database corrospoding to the username
+	   cout<<"enter the password of the respective data"<<password<<endl;
+	   //display the document if the password matches the required document
+   }
+
+};
+
+
+
+int main()
+{	
+
+	return 0;
+
+}
